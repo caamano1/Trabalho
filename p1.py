514e2b71f794768816cbd7e63f13cb83e239aab5
caamano1 <joaocaamano87@gmail.com>
2025-02-13 11:23:42 -0300

    Adicionado o radiobutton dos generos

--- p1.py
+++ p1.py
@@ -5,12 +5,17 @@ root.title('Trabalho')
 root.geometry('400x400')
 root.configure(bg='#004544')
 
-# Label for "Nome"
+#"Nome"
 tk.Label(root, text='Nome ', font=('Arial', 12), bg='#004544', fg='white').pack(
     padx=5, pady=5)
 
 entrada_nome = tk.Entry(root, width=35)
 entrada_nome.pack(padx=5, pady=5)
 
+#genero
+opcao = tk.StringVar(value='')
+tk.Label(root, text='Genero', font=('Arial', 12)).pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Masculino', variable=opcao, value='Masculino').pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Feminino', variable=opcao, value='Feminino').pack(padx=5, pady=5)
 
 root.mainloop()
