f13b1f0aed55112f239399a0c9d59b4ab61c8cfd
agathacristini <cristiniarmani@gmail.com>
2025-02-18 08:36:59 -0300

    Adicionado a opcao idade

--- p1.py
+++ p1.py
@@ -14,8 +14,13 @@ entrada_nome.pack(padx=5, pady=5)
 
 #genero
 opcao = tk.StringVar(value='')
-tk.Label(root, text='Genero', font=('Arial', 12)).pack(padx=5, pady=5)
-tk.Radiobutton(root, text='Masculino', variable=opcao, value='Masculino').pack(padx=5, pady=5)
-tk.Radiobutton(root, text='Feminino', variable=opcao, value='Feminino').pack(padx=5, pady=5)
+tk.Label(root, text='Genero', font=('Arial', 12), bg='#004544', fg='white').pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Masculino', variable=opcao, value='Masculino', bg='#004544', fg='white', font=('Arial',12)).pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Feminino', variable=opcao, value='Feminino', bg='#004544', fg='white', font=('Arial', 12)).pack(padx=5, pady=5)
+
+# idade 
+tk.Label(root, text="idade:", font=('Arial', 12), bg="#004544", fg="white").pack(padx=5, pady=5)
+entrada_idade = tk.Entry(root, width=5)
+entrada_idade.pack(padx=5, pady=5)
 
 root.mainloop()
