6384f8bcab2ac576598c726cde11a42b9739ef83
agathacristini <cristiniarmani@gmail.com>
2025-02-18 09:20:02 -0300

    Adicionado data de nasc e botao para mostrar info

--- p1.py
+++ p1.py
@@ -1,26 +1,42 @@
 import tkinter as tk
+from tkinter import messagebox
+
+def exibir_info():
+    nome = entrada_nome.get()
+    genero = opcao.get()
+    idade = entrada_idade.get()
+    nascimento = entrada_nascimento.get()
+    
+    info = f"Nome: {nome}\nGênero: {genero}\nIdade: {idade}\nData de Nascimento: {nascimento}"
+    messagebox.showinfo("Informações", info)
 
 root = tk.Tk()
 root.title('Trabalho')
 root.geometry('400x400')
-root.configure(bg='#004544')
+root.configure(bg='black')
 
-#"Nome"
-tk.Label(root, text='Nome ', font=('Arial', 12), bg='#004544', fg='white').pack(
-    padx=5, pady=5)
-
+# "Nome"
+tk.Label(root, text='Nome ', font=('Arial', 12), bg='black', fg='white').pack(padx=5, pady=5)
 entrada_nome = tk.Entry(root, width=35)
 entrada_nome.pack(padx=5, pady=5)
 
-#genero
-opcao = tk.StringVar(value='')
-tk.Label(root, text='Genero', font=('Arial', 12), bg='#004544', fg='white').pack(padx=5, pady=5)
-tk.Radiobutton(root, text='Masculino', variable=opcao, value='Masculino', bg='#004544', fg='white', font=('Arial',12)).pack(padx=5, pady=5)
-tk.Radiobutton(root, text='Feminino', variable=opcao, value='Feminino', bg='#004544', fg='white', font=('Arial', 12)).pack(padx=5, pady=5)
+# Gênero
+opcao = tk.StringVar(value='Não informado')
+tk.Label(root, text='Gênero', font=('Arial', 12), bg='black', fg='white').pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Masculino', variable=opcao, value='Masculino', bg='black', fg='white', font=('Arial', 12)).pack(padx=5, pady=5)
+tk.Radiobutton(root, text='Feminino', variable=opcao, value='Feminino', bg='black', fg='white', font=('Arial', 12)).pack(padx=5, pady=5)
 
-# idade 
-tk.Label(root, text="idade:", font=('Arial', 12), bg="#004544", fg="white").pack(padx=5, pady=5)
+# Idade
+tk.Label(root, text="Idade:", font=('Arial', 12), bg="black", fg="white").pack(padx=5, pady=5)
 entrada_idade = tk.Entry(root, width=5)
 entrada_idade.pack(padx=5, pady=5)
+
+# Data de nascimento
+tk.Label(root, text="Data de Nascimento:", font=('Arial', 12), bg='black', fg='white').pack(padx=5, pady=5)
+entrada_nascimento = tk.Entry(root, width=10)
+entrada_nascimento.pack(padx=5, pady=5)
+
+# Botão para exibir informações
+tk.Button(root, text="Mostrar Informações", command=exibir_info, font=('Arial', 12), bg='#008080', fg='white').pack(padx=5, pady=10)
 
 root.mainloop()
